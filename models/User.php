b90cec01c2e90f55f9e6f833b7fbdcda61fbbb5d
Carlos Oliva <colidom@outlook.com>
2023-10-23 19:26:37 +0200

    Añadido modelo de usuario

---
+++ models/User.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Model;
+
+class User extends ActiveRecord
+{
+    protected static $table = 'users';
+    protected static $columnsDB = ['id', 'name', 'surname', 'email', 'password', 'confirmed', 'token', 'admin'];
+
+    public $id;
+    public $name;
+    public $surname;
+    public $email;
+    public $password;
+    public $password2;
+    public $confirmed;
+    public $token;
+    public $admin;
+
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->name = $args['name'] ?? '';
+        $this->surname = $args['surname'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? '';
+        $this->confirmed = $args['confirmed'] ?? 0;
+        $this->token = $args['token'] ?? '';
+        $this->admin = $args['admin'] ?? 0;
+    }
+
+    // Validar el Login de Usuarios
+    public function validateLogin()
+    {
+        if (!$this->email) {
+            self::$alertas['error'][] = 'El Email del Usuario es Obligatorio';
+        }
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            self::$alertas['error'][] = 'Email no válido';
+        }
+        if (!$this->password) {
+            self::$alertas['error'][] = 'El Password no puede ir vacio';
+        }
+        return self::$alertas;
+    }
+
+    // Validación para cuentas nuevas
+    public function validateAccount()
+    {
+        if (!$this->name) {
+            self::$alertas['error'][] = 'El nombre es Obligatorio';
+        }
+        if (!$this->surname) {
+            self::$alertas['error'][] = 'El apellido es Obligatorio';
+        }
+        if (!$this->email) {
+            self::$alertas['error'][] = 'El Email es Obligatorio';
+        }
+        if (!$this->password) {
+            self::$alertas['error'][] = 'El Password no puede ir vacio';
+        }
+        if (strlen($this->password) < 6) {
+            self::$alertas['error'][] = 'El password debe contener al menos 6 caracteres';
+        }
+        if ($this->password !== $this->password2) {
+            self::$alertas['error'][] = 'Los password son diferentes';
+        }
+        return self::$alertas;
+    }
+
+    // Valida un email
+    public function validateEmail()
+    {
+        if (!$this->email) {
+            self::$alertas['error'][] = 'El Email es Obligatorio';
+        }
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            self::$alertas['error'][] = 'Email no válido';
+        }
+        return self::$alertas;
+    }
+
+    // Valida el Password
+    public function validatePassword()
+    {
+        if (!$this->password) {
+            self::$alertas['error'][] = 'El Password no puede ir vacio';
+        }
+        if (strlen($this->password) < 6) {
+            self::$alertas['error'][] = 'El password debe contener al menos 6 caracteres';
+        }
+        return self::$alertas;
+    }
+
+    /*  public function newPassword(): array
+    {
+        if (!$this->current_password) {
+            self::$alertas['error'][] = 'El Password Actual no puede ir vacio';
+        }
+        if (!$this->password_nuevo) {
+            self::$alertas['error'][] = 'El Password Nuevo no puede ir vacio';
+        }
+        if (strlen($this->password_nuevo) < 6) {
+            self::$alertas['error'][] = 'El Password debe contener al menos 6 caracteres';
+        }
+        return self::$alertas;
+    } */
+
+    // Comprobar el password
+    /*     public function checkPassword(): bool
+    {
+        return password_verify($this->current_password, $this->password);
+    } */
+
+    // Hashea el password
+    public function hashPassword(): void
+    {
+        $this->password = password_hash($this->password, PASSWORD_BCRYPT);
+    }
+
+    // Generar un Token
+    public function createToken(): void
+    {
+        $this->token = uniqid();
+    }
+}
