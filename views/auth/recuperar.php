f3cfd3214627097fbe44efb8b31d10e134347bfc
arques89 <arquesjavier@gmail.com>
2023-10-23 19:34:08 +0200

    Cambio de olvide contraseña y registro

--- views/auth/recuperar.php
+++ views/auth/recuperar.php
@@ -1,34 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recuperar contraseña</title>
-    <link rel="stylesheet" href="../../assets/css/recuperar.css">
-    <style>
-
-    </style>
-</head>
-
-<body>
+<main>
     <video autoplay muted loop>
-        <source src="../../assets/img/movie.mp4" type="video/mp4">
+        <!-- Reemplaza 'video.mp4' con la ruta de tu video -->
+        <source src="../assets/img/movie.mp4" type="video/mp4">
         Tu navegador no soporta el elemento de video.
     </video>
-    <div class="recuperar">
-        <h1>Recuperar</h1>
-        <form action="/registrar" method="post">
-            <div class="campo">
-                <label for="correo">Correo electrónico:</label>
-                <input type="email" name="correo" id="correo">
-            </div>
-            <button type="submit">Enviar email</button>
-            <div class="inferior">
-            <a href="/login">Volver</a>
-        </div>
-        </form>
-    </div>
-</body>
 
-</html>
+    <div id="contenedor">
+        <div id="central">
+            <div id="login">
+                <div class="titulo">
+                    Recuperar contraseña
+                </div>
+                <form method="POST" action="/login" id="loginform">
+                    <div class="campo">
+                        <label for="correo">Correo electrónico:</label>
+                        <input type="text" name="correo" placeholder="Correo Electronico" required>
+                    </div>
+                    <input type="submit" class="formulario__submit" value="Recuperar" />
+                </form>
+                <div class="pie-form">
+                    <a href="/login">¿Tienes Cuenta? Inicia sesión</a>
+                    <a href="/register">¿No tienes Cuenta? Registrate</a>
+                </div>
+            </div>
+            <div class="inferior">
+                <a href="/">Volver</a>
+            </div>
+        </div>
+    </div>
+</main>
