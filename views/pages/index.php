d2b5dd53e808feab9f371493cbde91ce08a50380
colidom <colidom@outlook.com>
2023-11-12 20:06:31 +0100

    Quitado fonde que molestaba y botón que no hacía nada

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,11 +1,7 @@
 <main>
 
-    <img src="../assets/img/peliculas_index.jpg" alt="" style="width:100%;">
 
     <h1>CarrotFlix</h1>
     <p>El servicio de streaming de películas y series de televisión para los amantes de las zanahorias.</p>
-    <button class="btn btn-primary btn-rounded">
-        Registrate ya en CarrotFlix
-    </button>
 
 </main>
