325ccc01938002195eac71306ea498b6ecb320a9
colidom <colidom@outlook.com>
2023-12-11 17:00:46 +0100

    Refactor debuguear, s, and esUltimo functions, and add isAuth and isAdmin functions
    Refactor the debuguear function to debug and provide a more descriptive
    comment explaining its purpose. Refactor the s function to sanitize and
    add a comment explaining its functionality. Refactor the esUltimo
    function to isLast and add comments to clarify its purpose.
    Additionally, add isAuth function to check if the user is authenticated
    and isAdmin function to check if the user is an administrator. These
    changes improve code clarity and maintainability. No issues referenced.

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,13 @@
 <?php
 
-function debuguear($variable): string
+/**
+ * Debugs a variable by displaying its contents and halting the script.
+ *
+ * @param mixed $variable The variable to debug.
+ *
+ * @return string Returns the debug output wrapped in HTML preformatted text.
+ */
+function debug($variable): string
 {
     echo '<pre>';
     var_dump($variable);
@@ -8,33 +15,54 @@ function debuguear($variable): string
     exit;
 }
 
-// Escapa / Sanitizar el HTML
-function s($html): string
+/**
+ * Escapes/Sanitizes HTML by converting special characters to HTML entities.
+ *
+ * @param string $html The HTML string to be sanitized.
+ *
+ * @return string Returns the sanitized HTML string with special characters converted to HTML entities.
+ */
+function sanitize($html): string
 {
-    $s = htmlspecialchars($html);
-
-    return $s;
+    return htmlspecialchars($html);
 }
 
-function esUltimo(string $actual, string $proximo): bool
+
+/**
+ * Checks if the given strings represent the last element in a sequence.
+ *
+ * @param string $current The current element in the sequence.
+ * @param string $next    The next element in the sequence.
+ *
+ * @return bool Returns true if the current element is not equal to the next element, otherwise returns false.
+ */
+function isLast(string $current, string $next): bool
 {
-    if ($actual !== $proximo) {
+    if ($current !== $next) {
         return true;
     } else {
         return false;
     }
 }
 
-// Función que revisa si el usuario está autenticado
+/**
+ * Checks if the user is authenticated.
+ *
+ * @return bool Returns true if the user is authenticated (session is set with 'name' key and not empty), otherwise returns false.
+ */
 function isAuth(): bool
 {
     if (!isset($_SESSION)) {
         session_start();
     }
-
-    return isset($_SESSION['name']) && !empty($_SESSION);
+    return isset($_SESSION['name']) && !empty($_SESSION['name']);
 }
 
+/**
+ * Checks if the user is an administrator.
+ *
+ * @return bool Returns true if the user is an administrator (session is set with 'isAdmin' key and not empty), otherwise returns false.
+ */
 function isAdmin(): bool
 {
     if (!isset($_SESSION)) {
