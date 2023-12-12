1ffe8d100baa008087591fbff445d21b75a73733
colidom <colidom@outlook.com>
2023-12-12 19:35:21 +0100

    Añadido campo image en la db

--- sql/crates.sql
+++ sql/crates.sql
@@ -46,6 +46,8 @@ CREATE TABLE `catalogue` (
   `rating` varchar(45) DEFAULT NULL,
   `cast` varchar(128) DEFAULT NULL,
   `language` varchar(45) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
