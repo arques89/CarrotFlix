80e3d6c849c07bdd0ff5ceabdb109f3a6b924603
colidom <colidom@outlook.com>
2023-12-12 21:41:08 +0100

    Cambiada versión de bootstrap y añadido enlace a cada carátula

--- views/catalogue/browse.php
+++ views/catalogue/browse.php
@@ -8,17 +8,8 @@ $basePath = dirname(__FILE__); // Obtiene la ruta absoluta del archivo actual
 <head>
     <meta charset="UTF-8">
     <title>Catálogo de Películas</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        /* Estilos adicionales */
-        .movie-card {
-            margin-bottom: 20px;
-        }
-
-        .movie-card img {
-            width: 100px;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -28,7 +19,7 @@ $basePath = dirname(__FILE__); // Obtiene la ruta absoluta del archivo actual
             <?php foreach ($movies as $movie) { ?>
                 <div class="col-md-2">
                     <div class="card movie-card">
-                        <img class="card-img-top" srcset="media/<?php echo $movie->getImage(); ?>" alt="<?php echo $movie->getTitle(); ?>">
+                        <a href=""><img class="card-img-top" srcset="media/<?php echo $movie->getImage(); ?>" alt="<?php echo $movie->getTitle(); ?>"></a>
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $movie->getTitle(); ?></h5>
                         </div>
