0765bdaef1efb05f59b2f5f876e505141ed6230f
colidom <colidom@outlook.com>
2023-12-12 21:48:07 +0100

    Añadido enlace al card de la carátula, para mejorar la accesibilidad de la vista

--- views/catalogue/browse.php
+++ views/catalogue/browse.php
@@ -19,13 +19,15 @@ $basePath = dirname(__FILE__); // Obtiene la ruta absoluta del archivo actual
             <?php foreach ($movies as $movie) { ?>
 
                 <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" srcset="media/<?php echo $movie->getImage(); ?>" alt="<?php echo $movie->getTitle(); ?>">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $movie->getTitle(); ?></h5>
-                        <p class="card-text"><?php $movie->getSynopsis(); ?></p>
-                        <a href="#" class="btn btn-primary">Ver película</a>
-                    </div>
+                    <a href="#">
+                        <img class="card-img-top" srcset="media/<?php echo $movie->getImage(); ?>" alt="<?php echo $movie->getTitle(); ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $movie->getTitle(); ?></h5>
+                            <p class="card-text"><?php $movie->getSynopsis(); ?></p>
+                        </div>
+                    </a>
                 </div>
+
             <?php } ?>
         </div>
     </div>
