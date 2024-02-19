70037425498d2f0c61904e281c7e2c48fa71b95b
Javier Arques Tortosa <arquesjavier@gmail.com>
2024-02-19 16:51:09 +0100

    Mergeando cambios Javier

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,3 +1,29 @@
-<main>
-    <p>El servicio de streaming de películas y series de televisión para los amantes de las zanahorias.</p>
-</main>
+
+<style>
+        html, body {
+            height: 100%;
+        }
+
+        main {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: url('../assets/img/fondo.jpeg') center/cover no-repeat;
+            margin: 0;
+        }
+
+        .contenido {
+            text-align: center;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <main class="d-flex">
+        <div class="container text-center">
+            <div class="contenido">
+            
+            </div>
+        </div>
+    </main>
--- views/templates/header.php
+++ views/templates/header.php
@@ -1,35 +1,109 @@
 <header class="header">
-    <div class="header__contenedor">
-        <nav class="header__navegacion">
-            <?php if (!isAuth()) {?>
+<nav class="navbar navbar fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">CarrotFlix</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon">
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ec5353" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M4 6l16 0" />
+  <path d="M4 12l16 0" />
+  <path d="M4 18l16 0" />
+</svg>
+
+      </span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+
+            <?php if (empty($_SESSION)) { ?>
+                <a href="/register" class="header__enlace">Register</a>
+            <?php } ?>
+            
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+            <?php if (!isAuth()) { ?>
                 <a href="/login" class="header__enlace">Iniciar sesión</a>
-            <?php }?>
-            <?php if (!empty($_SESSION)) {?>
+            <?php } ?>
+            <?php if (!empty($_SESSION)) { ?>
                 <form method="POST" action="/logout" class="dashboard__form">
                     <input type="submit" value="Cerrar Sesión" class="dashboard__submit--logout">
                 </form>
-            <?php }?>
-            <?php if (empty($_SESSION)) {?>
+                <?php } ?>
+            </a>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <?php if (!isAuth()) { ?>
+                <a href="/login" class="header__enlace">Iniciar sesión</a>
+            <?php } ?>
+            <?php if (!empty($_SESSION)) { ?>
+                <form method="POST" action="/logout" class="dashboard__form">
+                    <input type="submit" value="Cerrar Sesión" class="dashboard__submit--logout">
+                </form>
+            <?php } ?>
+            <?php if (empty($_SESSION)) { ?>
                 <a href="/register" class="header__enlace">Register</a>
-            <?php }?>
+            <?php } ?>
 
         </nav>
 
-        <div class="header__contenido">
-            <a href="/">
-                <h1 class="header__logo">CarrotFlix</h1>
-            </a>
-        </div>
     </div>
 </header>
 <div class="barra">
     <div class="barra__contenido">
         <a href="/">
             <h2 class="barra__logo">
-                <?php if (isAuth()) {?>
-                    Bienvenido: <span><?php echo $_SESSION['name']; ?></span>
-                    Rol: <span><?php echo isAdmin() ? 'Admin' : 'User'; ?></span>
-                <?php }?>
+                <?php if (isAuth()) { ?>
+                    Bienvenido: <span><?php echo $_SESSION['name'];?></span> 
+                    Rol: <span><?php echo isAdmin() ? "Admin" : "User"; ?></span>  
+                <?php } ?>
             </h2>
         </a>
 
