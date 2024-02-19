185ea741fe6663ec49e9e6bfcdfde0734e06b8c3
Javier Arques Tortosa <arquesjavier@gmail.com>
2024-02-19 19:34:29 +0100

    Header, añadido bienvenido con nombre, colores y browser añadido

--- views/templates/header.php
+++ views/templates/header.php
@@ -2,20 +2,41 @@
 <nav class="navbar navbar fixed-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">CarrotFlix</a>
+
+      <?php if (!empty($_SESSION)) { ?>
+      <?php if (isAuth()) { ?>  
+        <a id="browse" class="navbar-brand" href="/browse">Browse</a>
+      <?php } ?>
+      <?php } ?>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
       <span class="navbar-toggler-icon">
       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ec5353" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M4 6l16 0" />
-  <path d="M4 12l16 0" />
-  <path d="M4 18l16 0" />
-</svg>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M4 6l16 0" />
+        <path d="M4 12l16 0" />
+        <path d="M4 18l16 0" />
+      </svg>
 
       </span>
     </button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+           
+          
+          <?php if (empty($_SESSION)) { ?>
+              <?php if (!isAuth()) { ?>
+                Bienvenido
+                <?php } ?>
+                <?php } ?>
+                <?php if (!empty($_SESSION)) { ?>
+                  <?php if (isAuth()) { ?>
+                    Bienvenido <?php echo $_SESSION['name'] . ' ' . $_SESSION['surname'] ?>
+                    <?php } ?>
+                <?php } ?>
+          
+        </h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
@@ -24,7 +45,9 @@
             <a class="nav-link active" aria-current="page" href="#">
 
             <?php if (empty($_SESSION)) { ?>
+              <?php if (!isAuth()) { ?>
                 <a href="/register" class="header__enlace">Register</a>
+                <?php } ?>
             <?php } ?>
             
             </a>
@@ -32,7 +55,7 @@
           <li class="nav-item">
             <a class="nav-link active" href="#">
             <?php if (!isAuth()) { ?>
-                <a href="/login" class="header__enlace">Iniciar sesión</a>
+                <a href="/login" class="header__enlace">Login</a>
             <?php } ?>
             <?php if (!empty($_SESSION)) { ?>
                 <form method="POST" action="/logout" class="dashboard__form">
