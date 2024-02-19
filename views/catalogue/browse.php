8055e9f822d9a56900bfdb0ee6e005e20b96c3b4
Javier Arques Tortosa <arquesjavier@gmail.com>
2024-02-19 20:13:46 +0100

    Añadidas peliculas al browser y eliminado de css Overflow: hidden y fotter abajo

--- views/catalogue/browse.php
+++ views/catalogue/browse.php
@@ -1,11 +1,11 @@
 <main>
-    <div class="container">
-        <h1>Catálogo de Películas</h1>
+    <div class="container mt-5">
+        <h1 class="" >Catálogo de Películas</h1>
         <div class="row">
             <?php foreach ($movies as $movie) { ?>
                 <div class="card" style="width: 18rem;">
                     <a href="#">
-                        <img class="card-img-top" srcset="media/<?php echo $movie->getImage(); ?>" alt="<?php echo $movie->getTitle(); ?>">
+                        <img class="card-img-top" srcset="media/<?php echo $movie->getImageURL(); ?>" alt="<?php echo $movie->getTitle(); ?>">
                     </a>
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $movie->getTitle(); ?></h5>
