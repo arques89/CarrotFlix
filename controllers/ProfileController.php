ed65d97a7c97f6c6a08f7f06ed055c63d3f5a03d
colidom <colidom@outlook.com>
2024-02-20 19:09:01 +0100

    Conseguido insertar todos los registros del csv en db

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -31,45 +31,38 @@ class ProfileController
             if (isset($_FILES['csvFile']) && $_FILES['csvFile']['error'] === UPLOAD_ERR_OK) {
                 $csvFile = $_FILES['csvFile']['tmp_name'];
 
-                // Ejemplo básico:
                 $file = fopen($csvFile, 'r');
                 while (($data = fgetcsv($file, 1000, ';')) !== FALSE) {
-                    // Procesar cada línea del archivo CSV
-                    // Aquí puedes agregar el código para insertar los datos en la base de datos
 
-                    // Crear una nueva instancia de la película con los datos del CSV
                     $newMovie = new Movie([
-                        'title' => $data[1],      // Ajusta los índices según la estructura de tu CSV
-                        'director' => $data[2],
-                        'year' => $data[3],
-                        'genre' => $data[4],
-                        'synopsis' => $data[5],
-                        'rating' => $data[6],
-                        'cast' => $data[7],
-                        'language' => $data[8],
-                        'image_url' => $data[9],
+                        'title' => $data[0],
+                        'director' => $data[1],
+                        'year' => $data[2],
+                        'genre' => $data[3],
+                        'synopsis' => $data[4],
+                        'rating' => $data[5],
+                        'cast' => $data[6],
+                        'language' => $data[7],
+                        'image_url' => $data[8],
                     ]);
 
                     $result = $newMovie->save();
-
-                    if ($result['result']) {
-
-                        session_start();
-                        $userId = $_SESSION['id'];
-                        $user = User::where('id', $userId);
-
-                        $router->render('profile/index', [
-                            'title' => 'Perfil de usuario',
-                            'user' => $user,
-                            'alerts' => 'Registros insertados correctamente.'
-                        ]);
-                    } else {
-                        echo "Error al insertar la película '" . $data[0] . "'. Datos no válidos.<br>";
-                    }
                 }
                 fclose($file);
 
-                echo "Archivo CSV cargado y procesado correctamente.";
+                if ($result['result']) {
+                    session_start();
+                    $userId = $_SESSION['id'];
+                    $user = User::where('id', $userId);
+
+                    $router->render('profile/index', [
+                        'title' => 'Perfil de usuario',
+                        'user' => $user,
+                        'alerts' => 'Registros insertados correctamente.'
+                    ]);
+                } else {
+                    echo "Error al insertar la película '" . $data[0] . "'. Datos no válidos.<br>";
+                }
             } else {
                 echo "Error al cargar el archivo CSV.";
             }
