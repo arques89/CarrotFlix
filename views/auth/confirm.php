71481253657f8a30c9a8848cc76f7339a5ff8962
Javier Arques Tortosa <arquesjavier@gmail.com>
2024-03-07 05:26:46 +0100

    Eliminada la linea de titulo

--- views/auth/confirm.php
+++ views/auth/confirm.php
@@ -5,10 +5,9 @@ header('Refresh: 5; url=./login');
 ?>
 <main class="auth">
     <div class="message">
-        <h2 class="auth__heading"><?php echo $title; ?></h2>
         <div>
             <p class="auth__texto">
-                <h4>Usuario Confirmado.</h4> 
+                <h2>Usuario Confirmado.</h2> 
             </p>
             <img src="../assets/img/logo-check.avif" alt="">
             <h3>Serás redirigido a la página de Login. Gracias por verificar tu usuario.</h3>
